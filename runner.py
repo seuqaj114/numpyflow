4287a166e0318592103d28a03702fbce10ef7a6f
Miguel <migjaques@gmail.com>
2018-06-03 22:49:15 +0100

    Implement constants, matmul, transpose and shapes

--- runner.py
+++ runner.py
@@ -8,6 +8,7 @@ class Graph:
         self.operations = []
         self.placeholders = []
         self.variables = []
+        self.constants = []
 
     def as_default(self):
         global _default_graph
@@ -65,7 +66,12 @@ class Op:
         _default_graph.operations.append(self)
 
     def execute(self):
-        pass
+        self.validate_inputs()
+        self.execute_specific()
+
+    def execute_specific(self):
+        # To be implemented by each specific operation
+        raise NotImplementedError
 
     def reset_value(self):
         self.value = None
@@ -81,17 +87,48 @@ class Op:
 
 class AddOp(Op):
 
-    def execute(self):
-        self.validate_inputs()
-        self.value = sum(inp.value for inp in self.inputs)
+    def __init__(self, *args):
+
+        assert len(args) == 2
+        super(AddOp, self).__init__(*args)
+        # Get shape by running dummy operation with shape of inputs
+        # This also allows validation of input shapes
+        self.shape = (np.zeros(args[0].shape) + np.zeros(args[1].shape)).shape
+
+    def execute_specific(self):
+        self.value = self.inputs[0].value + self.inputs[1].value
+
+
+class matmul(Op):
+
+    def __init__(self, *args):
+
+        assert len(args) == 2
+        super(matmul, self).__init__(*args)
+        self.shape = np.dot(np.zeros(args[0].shape),np.zeros(args[1].shape)).shape
+
+    def execute_specific(self):
+        self.value = np.dot(self.inputs[0].value, self.inputs[1].value)
+
+
+class transpose(Op):
+
+    def __init__(self, *args):
+
+        assert len(args) == 1
+        super(transpose, self).__init__(*args)
+        self.shape = np.transpose(args[0]).shape
+
+    def execute_specific(self): 
+        self.value = np.transpose(self.inputs[0].value)
 
 
 class Variable:
     # Variable class - we try to copy numpy's operations
-    def __init__(self, name, shape, initial_value=0.0):
+    def __init__(self, name, shape, value=0.0):
         assert isinstance(shape, list) or isinstance(shape, tuple)
 
-        self.value = initial_value*np.ones(shape)
+        self.value = value*np.ones(shape)
         self.shape = shape
         self.name = name
 
@@ -107,7 +144,27 @@ class Variable:
         return self.__str__()
 
 
-class Placeholder():
+class Constant:
+
+    def __init__(self, value):
+        assert type(value) in [int, float, np.ndarray], "%s" % type(value)
+
+        self.value = np.array(value)
+        self.shape = np.shape(value)
+
+        _default_graph.variables.append(self)
+
+    def __add__(self, obj):
+        return AddOp(self, obj)
+
+    def __str__(self):
+        return "<Constant %s, shape %s, at %s>" % (self.value, self.shape, hex(id(self)))
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Placeholder:
 
     def __init__(self, name, shape):
         self.shape = shape
@@ -134,12 +191,13 @@ class Placeholder():
 
 Graph().as_default()
 
-x = Placeholder("x", (2,3))
+x = Placeholder("x", shape=(2,3))
 a = Variable("a", (2,3), 1.0)
 b = Variable("b", (2,3), 2.0)
 
-c = a + b
+c = a + b + Constant(1)
 d = c + b + x
+e = matmul(a, transpose(x))
 
 print(_default_graph.operations)
 print(_default_graph.variables)
@@ -148,3 +206,4 @@ sess = Session()
 # Fetch result of c
 print(sess.run(c))
 print(sess.run(d, {x: np.array([[1,2,3],[4,5,6]])}))
+print(sess.run(e, {x: np.array([[1,2,3],[4,5,6]])}))
